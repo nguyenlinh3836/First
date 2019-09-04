d6e5ec019147ae67d64a1b945be5315a05fd64b0
nguyenlinh3836 <54592412+nguyenlinh3836@users.noreply.github.com>
2019-09-04 20:22:09 +0700

    Baitap3

---
+++ giaipt2.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	float a,b,c,delta,x1,x2;
+	printf("nhap a,b,c\n");
+	scanf("%f",&a);
+	scanf("%f",&b);
+	scanf("%f",&c);
+		delta = b*b-4*a*c;
+		if (a!=0){
+		if (delta < 0){
+			printf ("phuong trinh vo nghiem\n");
+		}else if(delta==0) {
+			x1=x2=-b/(2*a);
+			printf("phuong trinh co nghiem kep %f\n",x1);		
+		}else{
+			delta= sqrt(delta);
+			x1= ((-b+delta)/(2*a));
+			x2=((-b-delta)/(2*a));
+			printf("phuong trinh co 2 nghiem phan biet x1=%f\n ",x1);
+			printf("phuong trinh co 2 nghiem phan biet x2=%f\n",x2);
+		}
+}else{
+	printf("khong phai la phuong trinh bac hai");
+}
+			return 0;		
+	}
