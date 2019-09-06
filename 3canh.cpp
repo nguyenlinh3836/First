c114db2ecc5ed59a92a8d57676ac3584e5c4e828
nguyenlinh3836 <54592412+nguyenlinh3836@users.noreply.github.com>
2019-09-06 13:24:45 +0700

    BT4

---
+++ 3canh.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+	int a,b,c;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	scanf("%d",&c);
+	while(a+b<=c || b+c<=a || c+a<=b){
+		printf("moi nhap lai\n");
+		scanf("%d",&a);
+	    scanf("%d",&b);
+	    scanf("%d",&c);
+		}
+		printf("ba canh tam giac");	
+	return 0;
+}
+
